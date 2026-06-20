7f267644e1d7751a9c7efccc8ccea8ed701f8cd1
sonubaliyan <46953107+sonubaliyan@users.noreply.github.com>
2026-06-20 11:19:58 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<vector<string>> res;
+
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+
+        // Check upper column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void backtrack(int row, vector<string>& board, int n) {
+
+        if (row == n) {
+            res.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                board[row][col] = 'Q';
+
+                backtrack(row + 1, board, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<string> board(n, string(n, '.'));
+
+        backtrack(0, board, n);
+
+        return res;
+    }
+};
